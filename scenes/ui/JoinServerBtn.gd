b143b8a903b1ff1574b4ce540c8fd3cff89ac64a
mjmorales <morales.jmanuel16@gmail.com>
2020-06-09 05:37:37 -0400

    Added some example animations to a player.

--- scenes/ui/JoinServerBtn.gd
+++ scenes/ui/JoinServerBtn.gd
@@ -4,6 +4,10 @@ onready var Lobby = $"/root/Lobby"
 onready var Network = $"/root/Network"
 
 func _on_JoinServerBtn_pressed():
-	Network.join_server("127.0.0.1")
+	var server_ip_edit = get_parent().get_node("JoinServerIPEdit")
+	var server_ip = "127.0.0.1"
+	if server_ip_edit.text:
+		server_ip = server_ip_edit.text
+	Network.join_server(server_ip)
 	Lobby.register_player(Lobby.my_info)
 	get_tree().change_scene("res://scenes/ui/PreGameLobby.tscn")
